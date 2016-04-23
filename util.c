5c90b48f2510e9b49da243febe6c20250068e0e2
Domagoj Stolfa <domagoj.stolfa@gmail.com>
2016-04-23 17:30:46 +0200

    Added quotes for WPA strings

--- util.c
+++ util.c
@@ -1,5 +1,9 @@
+#include <stdio.h>
+
 #include <stdlib.h>
 #include <string.h>
+
+#include "util.h"
 
 void
 set_str(char **dest, const char *src)
@@ -19,5 +23,35 @@ set_str(char **dest, const char *src)
 
 	if (src_len <= dest_len) {
 		if (strcpy(*dest, src) == NULL) { exit(1); }
+	}
+}
+
+/* TODO: This can be done better */
+void
+set_str_quote(char **dest, const char *src)
+{
+	int src_len;
+	int dest_len;
+	
+	if (src == NULL) return;
+	src_len = strlen(src);
+
+	
+	if (*dest == NULL) {
+		if ((*dest = malloc(src_len+2)) == NULL) exit(1);
+		dest_len = src_len+2;
+	} else {
+		dest_len = strlen(*dest);
+	}	
+
+	/* Dragons be here */
+	if (src_len+2 <= dest_len) {
+		(*dest)++;
+		if (strcpy(*dest, src) == NULL) { exit(1); }
+		(*dest)--;
+		**dest = '\"';
+		*dest = (*dest) + src_len+1;
+		**dest = '\"';
+		*dest = (*dest) - src_len-1;
 	}
 }
--- util.h
+++ util.h
@@ -2,5 +2,6 @@
 #define UTIL_H
 
 void set_str(char **dest, const char *src);
+void set_str_quote(char **dest, const char *str);
 
 #endif
